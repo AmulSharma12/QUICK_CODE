94747ad4a891e3e9f199897f0b2d72414fd63a11
Amul Op <amulsharmarr67@gmail.com>
2021-12-28 12:57:54 +0530

    Printing all subsequences

---
+++ S2-Recursion/NewRecursion/S11_PrintingAllSubsequence.java
@@ -0,0 +1,33 @@
+// printing all the subsequences 
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class S11_PrintingAllSubsequence
+{
+	public static void main(String[] args)
+	{
+		int[] arr = {1,3,2};
+		ArrayList<Integer> ds = new ArrayList<>();
+		subSeq(arr,0,ds,arr.length);
+	}
+
+
+	public static void subSeq(int[] arr,int index,ArrayList<Integer> ds,int n)
+	{
+		if(index == n)
+		{
+			System.out.println(ds);
+			return;
+		}
+
+
+		//pick logic
+		ds.add(arr[index]);
+		subSeq(arr, index+1, ds, n);
+		ds.remove(ds.size()-1);
+
+
+		//not pick logic
+		subSeq(arr, index+1, ds, n);
+	}
+}
